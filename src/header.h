8f8eb3c17398b147a8dcd080f39eca08b1cc03c9
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2026-01-28 22:53:41 +0300

    feat: add system functions in header

--- src/header.h
+++ src/header.h
@@ -38,3 +38,8 @@ const char *getPassword(struct User u);
 void createNewAcc(struct User u);
 void mainMenu(struct User u);
 void checkAllAccounts(struct User u);
+void updateAccount(struct User u);
+void makeTransaction(struct User u);
+void checkAccountDetails(struct User u);
+void removeAccount(struct User u);
+void transferOwner(struct User u);
--- src/main.c
+++ src/main.c
@@ -22,27 +22,22 @@ void mainMenu(struct User u)
         createNewAcc(u);
         break;
     case 2:
-        // student TODO : add your **Update account information** function
-        // here
+        updateAccount(u);
         break;
     case 3:
-        // student TODO : add your **Check the details of existing accounts** function
-        // here
+        checkAccountDetails(u);
         break;
     case 4:
         checkAllAccounts(u);
         break;
     case 5:
-        // student TODO : add your **Make transaction** function
-        // here
+        makeTransaction(u);
         break;
     case 6:
-        // student TODO : add your **Remove existing account** function
-        // here
+        removeAccount(u);
         break;
     case 7:
-        // student TODO : add your **Transfer owner** function
-        // here
+        transferOwner(u);
         break;
     case 8:
         exit(1);
@@ -81,8 +76,7 @@ void initMenu(struct User *u)
             r = 1;
             break;
         case 2:
-            // student TODO : add your **Registration** function
-            // here
+            registerMenu(u->name, u->password);
             r = 1;
             break;
         case 3:
